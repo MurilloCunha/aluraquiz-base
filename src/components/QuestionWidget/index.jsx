65af237a9afeeddddeb2c87b2e7895199354f7d0
MurilloCunha <jcunhamurillo@gmail.com>
2021-02-01 09:46:29 -0300

    QuestionWidget

---
+++ src/components/QuestionWidget/index.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable no-unused-expressions */
+import React, { useState } from 'react';
+import Proptypes from 'prop-types';
+import Widget from '../Widget';
+import Button from '../Button';
+import AlternativesForm from '../AlternativesForm';
+
+function QuestionWidget({
+  question,
+  questionIndex,
+  totalQuestions,
+  onSubmit,
+  addResult,
+}) {
+  const [selectedAlternative, setSelectedAlternative] = useState(undefined);
+  const [isQuestionSubmited, setIsQuestionSubmited] = useState(false);
+  const questionId = `question__${questionIndex}`;
+  const isCorrect = selectedAlternative === question.answer;
+  const hasAlternativSelected = selectedAlternative !== undefined;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <AlternativesForm
+          onSubmit={(infosDoEvento) => {
+            infosDoEvento.preventDefault();
+            setIsQuestionSubmited(true);
+            setTimeout(() => {
+              addResult(isCorrect);
+              onSubmit();
+              setIsQuestionSubmited(false);
+              setSelectedAlternative(undefined);
+            }, 1 * 1000);
+          }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            const alternativeStatus = isCorrect ? 'SUCCESS' : 'ERROR';
+            const isSelected = selectedAlternative === alternativeIndex;
+            return (
+              <Widget.Topic
+                as="label"
+                key={alternativeId}
+                htmlFor={alternativeId}
+                data-selected={isSelected}
+                data-status={isQuestionSubmited && alternativeStatus}
+              >
+                <input
+                  id={alternativeId}
+                  name={questionId}
+                  onChange={() => setSelectedAlternative(alternativeIndex)}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+          <Button type="submit" disabled={!hasAlternativSelected}>
+            Confirmar
+          </Button>
+          {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
+          {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
+        </AlternativesForm>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+export default QuestionWidget;
+
+QuestionWidget.propTypes = {
+  question: Proptypes.objectOf(Proptypes.any).isRequired,
+  questionIndex: Proptypes.number.isRequired,
+  totalQuestions: Proptypes.number.isRequired,
+  onSubmit: Proptypes.func.isRequired,
+  addResult: Proptypes.func.isRequired,
+};
