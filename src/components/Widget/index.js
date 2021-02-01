a19d52a638f49e2e22f67c0b5713b019b6dffda1
MurilloCunha <jcunhamurillo@gmail.com>
2021-02-01 09:47:55 -0300

    Widget

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Widget = styled.div`
   margin-top: 24px;
@@ -29,6 +29,27 @@ Widget.Header = styled.header`
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
+`;
+Widget.Text = styled.p`
+  font-size: 0.8rem;
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: 0.3s;
+  display: block;
+
+  &:hover,
+  &:focus {
+    opacity: 0.5;
+  }
 `;
 
 export default Widget;
